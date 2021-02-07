a495c90c87580fc4fa86e4d151f2a2465d450218
Andrei Racasan <aracasan@outlook.com>
2021-02-07 22:26:07 +0000

    build countries list comp and display data

--- src/comps/CountriesList/CountriesList.js
+++ src/comps/CountriesList/CountriesList.js
@@ -1,5 +1,23 @@
+import styles from './CountriesList.module.css'
+
 const CountriesList = ({countries}) => {
-  return <div>Countries</div>
+  return <div>
+    
+    <div className={styles.heading}>
+      <button className={styles.heading_name}>
+        <div>Name</div>
+      </button>
+
+      <button className={styles.heading_population}>
+        <div>Population</div>
+      </button>
+    </div>
+
+    {countries.map(c => <div className={styles.row}>
+      <div className={styles.name}>{c.name}</div>
+      <div className={styles.population}>{c.population}</div>
+    </div>)}
+  </div>
 }
 
 export default CountriesList;
